141596f19dbc735576c182525bb5fefb76c5b1f2
Sean Clayton <sean@seanclayton.me>
2019-02-23 22:22:52 -0500

    Refine Quest model

--- Assets/Scripts/Primitives/CTQuest.cs
+++ Assets/Scripts/Primitives/CTQuest.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CTQuest
+public class CTQuest : MonoBehaviour
 {
+    public CTQuestId id;
     public CTQuestStatus status { get; private set; } = CTQuestStatus.Ready;
     public bool assigned { get; private set; } = false;
+    [SerializeField] public string title;
+    [SerializeField] public string description;
 
     public void Assign() => assigned = true;
-
     public void MarkFinished() => status = CTQuestStatus.Finished;
-
     public void MarkFailed() => status = CTQuestStatus.Failed;
 }
---
+++ Assets/Scripts/Primitives/CTQuestId.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CTQuestId
+{
+    private string id;
+
+    public CTQuestId(string newId) => id = newId;
+
+    public override string ToString() => id;
+}

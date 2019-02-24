1e996aed6206704bbddfea3728b5f7d0ab3af2cd
Sean Clayton <sean@seanclayton.me>
2019-02-24 13:27:02 -0500

    Initial work for main menu

---
+++ Assets/Scripts/UI/CTMainMenuButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CTMainMenuButton : MonoBehaviour
+{
+
+    Button myButton;
+    private TextMeshProUGUI text;
+    [SerializeField] public bool isDisabled = true;
+
+    void Awake()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        if (!isDisabled)
+            text.color = new Color(255, 255, 255, 0.5f);
+        else
+            text.color = new Color(255, 255, 255, 0.1f);
+    }
+
+    public void OnHover()
+    {
+        if (!isDisabled)
+            text.color = new Color(255, 255, 255, 1.0f);
+    }
+
+    public void OnUnHover()
+    {
+        if (!isDisabled)
+            text.color = new Color(255, 255, 255, 0.5f);
+    }
+
+    public void Disable()
+    {
+        isDisabled = true;
+        text.color = new Color(255, 255, 255, 0.1f);
+    }
+
+    public void Enable()
+    {
+        isDisabled = false;
+        text.color = new Color(255, 255, 255, 0.5f);
+    }
+
+}
---
+++ Assets/Scripts/UI/CTMainMenuContinueButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CTMainMenuContinueButton : CTMainMenuButton
+{
+    void Awake()
+    {
+        isDisabled = true;
+    }
+}

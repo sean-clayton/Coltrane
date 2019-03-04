1648cfb48db7ea9a2f299b43f3fab56663780f11
Sean Clayton <sean@seanclayton.me>
2019-03-03 20:55:41 -0500

    Add top down camera script init

---
+++ Assets/Scripts/Cameras/CTTopDownCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coletrane.Cameras
+{
+    public class CTTopDownCamera : MonoBehaviour
+    {
+        public Transform target;
+        public float height = 10f;
+        public float distance = 20f;
+        public float angle = 45f;
+
+        #region Main Methods
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        #endregion
+
+        #region Helper Methods
+        protected virtual void HandleCamera()
+        {
+
+        }
+        #endregion
+    }
+}
